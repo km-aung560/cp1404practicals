f157dec2918e9a8ee5e32f3ccfab1876fad8dbc8
km-aung560 <kaungmin.aung@my.jcu.edu.au>
2022-09-27 21:26:41 +1000

    Add Random Numbers file to repository

---
+++ prac_03/randoms.py
@@ -0,0 +1,19 @@
+import random
+# Question 1
+#   - prints random number
+#   - 5, 19
+
+# Question 2
+#   - prints random odd number
+#   - 3, 9
+#   - No, line 2 cannot produce a 4
+
+# Question 3
+#   - prints a random float-type number
+#   - 2.5, 5.499
+
+# Question 4
+random_number = random.randint(1, 101)
+print(random_number)
+
+
