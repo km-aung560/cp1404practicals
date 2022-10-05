d8dfed07e99ae754318e62cca22fdedb6e971ba7
km-aung560 <kaungmin.aung@my.jcu.edu.au>
2022-10-05 19:18:53 +1000

    Add lists_warmup.py file to prac 04

---
+++ lists_warmup.py
@@ -0,0 +1,25 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+"""
+numbers[0] = 3
+numbers[-1] = 9
+numbers[3] = 1
+numbers[:-1] = [3, 1, 4, 1, 5, 9]
+numbers[3:4] = [1]
+5 in numbers = True
+7 in numbers = False
+"3" in numbers = False
+numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+"""
+# Question 1
+numbers[0] = "ten"
+
+# Question 2
+numbers[-1] = 1
+
+# Question 3
+print(numbers[2:])
+
+# Question 4
+print(9 in numbers)
+
