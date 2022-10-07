626c6248c6933a948cdc399681d460a4ec865ba5
km-aung560 <kaungmin.aung@my.jcu.edu.au>
2022-10-07 10:47:17 +1000

    Improve subject_reader.py based on nested lists and functions

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,13 +7,15 @@ FILENAME = "subject_data.txt"
 
 
 def main():
+    """Read subject data and display neatly. """
     data = get_data()
-    print(data)
+    display_subjects(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    rows = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -22,8 +24,16 @@ def get_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        rows.append(parts)
         print("----------")
     input_file.close()
+    return rows
+
+
+def display_subjects(data):
+    """Display subject data nicely. """
+    for subject_data in data:
+        print("{} is taught by {:12} and has {:3} students".format(*subject_data))
 
 
 main()
