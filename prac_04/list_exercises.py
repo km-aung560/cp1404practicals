bc4702cacfffd859ffc1f0e86ddbe06b60902d93
km-aung560 <kaungmin.aung@my.jcu.edu.au>
2022-10-07 11:00:04 +1000

    Add list_exercises.py to prac 04

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,14 @@
+MAXIMUM_NUMBERS = 5
+numbers = [5, 20, 1, 2, 3]
+# for i in range(MAXIMUM_NUMBERS):
+#     number = int(input("Number: "))
+#     numbers.append(number)
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[MAXIMUM_NUMBERS - 1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+
+
+
