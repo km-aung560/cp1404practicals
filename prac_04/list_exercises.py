2c1750264e73f07098647175622ea08e6ccbca23
km-aung560 <kaungmin.aung@my.jcu.edu.au>
2022-10-07 11:09:19 +1000

    Update list_exercises.py to include titles and new program

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,3 +1,5 @@
+""" Basic List Operations """
+
 MAXIMUM_NUMBERS = 5
 numbers = [5, 20, 1, 2, 3]
 # for i in range(MAXIMUM_NUMBERS):
@@ -8,6 +10,18 @@ print(f"The last number is {numbers[MAXIMUM_NUMBERS - 1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+""" Woefully inadequate security checker """
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Username: ")
+if username in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied")
+
 
 
 
