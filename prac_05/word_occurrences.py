5f9adf171ed3d4d0ec6207f8280e20b7a0aee0e1
km-aung560 <kaungmin.aung@my.jcu.edu.au>
2022-10-15 19:16:28 +1000

    Fix word occurrences program based on code review

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -6,7 +6,6 @@ Actual: 25 minutes
 """
 word_to_count = {}
 text = input("Text: ")
-print(text)
 words = text.split()
 for word in words:
     frequency = word_to_count.get(word, 0)
@@ -19,10 +18,3 @@ words.sort()
 max_length = max(len(words) for word in words)
 for word in words:
     print(f"{word:{max_length}} : {word_to_count[word]}")
-
-
-
-
-
-
-
