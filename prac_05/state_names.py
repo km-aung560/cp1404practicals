31d9429fb8fbeae984eb0ada2ac0b8ac0b76726d
km-aung560 <kaungmin.aung@my.jcu.edu.au>
2022-10-15 19:15:41 +1000

    Fix state names program based on code review

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -8,11 +8,11 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
 print(CODE_TO_NAME)
 
 state_code = input("Enter short state: ").upper()
-while state_code != "":
+while not state_code:
     try:
         print(state_code, "is", CODE_TO_NAME[state_code])
     except KeyError:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
 for state_code in CODE_TO_NAME:
-    print(f"{state_code:{3}} is {CODE_TO_NAME[state_code]}")
+    print(f"{state_code:3} is {CODE_TO_NAME[state_code]}")
