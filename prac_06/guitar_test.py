2db064b7d99b989b15ac07958ac370175a0c2062
km-aung560 <kaungmin.aung@my.jcu.edu.au>
2022-10-29 19:46:43 +1000

    Rename age function

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -3,8 +3,8 @@ from prac_06.guitar import Guitar
 guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
 other = Guitar("Another Guitar", 2013, 15000)
 
-print(f"{guitar.name} get_age() - Expected 100. Got {guitar.get_age()}")
-print(f"{other.name} get_age() - Expected 9. Got {other.get_age()}")
+print(f"{guitar.name} get_age() - Expected 100. Got {guitar.calculate_age()}")
+print(f"{other.name} get_age() - Expected 9. Got {other.calculate_age()}")
 print(f"{guitar.name} is_vintage() - Expected True. Got {guitar.is_vintage()}")
 print(f"{other.name} is_vintage() - Expected False. Got {other.is_vintage()}")
 
