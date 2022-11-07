cffb6ba6b5f9ad6fbdbb78e4a26742bfda134a07
km-aung560 <kaungmin.aung@my.jcu.edu.au>
2022-11-07 18:43:35 +1000

    Add Project Management file to prac 7, include load-file function

---
+++ prac_07/project_management.py
@@ -0,0 +1,49 @@
+"""Project Management
+Estimate: 60 mins
+Actual:
+
+"""
+from prac_07.project import Project
+
+MENU = "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new " \
+       "project\n- (U)pdate project\n- (Q)uit "
+FILENAME = "projects.txt"
+HEADER = "Name	Start Date	Priority	Cost Estimate	Completion Percentage"
+def main():
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            # filename = input("filename: ")
+            projects = load_file()
+            print(projects)
+
+        elif choice == "S":
+            pass
+        elif choice == "D":
+            # display_projects()
+        elif choice == "F":
+            pass
+        elif choice == "A":
+            pass
+        elif choice == "U":
+            pass
+        else:
+            print("Invalid input")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Farewell")
+
+
+def load_file():
+    with open(FILENAME, encoding="UTF8") as in_file:
+        projects = [line.strip().replace("\t", ",").split(',') for line in in_file]
+        projects.remove(projects[0])
+    return projects
+
+
+    # def display_projects():
+    #     print(Project)
+
+
+main()
