c7658d3a10e18fbc78aaca8e38d5ab4ee7496282
km-aung560 <kaungmin.aung@my.jcu.edu.au>
2022-11-17 20:58:00 +1000

    Modify taxi test code to accommodate changes in taxi class

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -3,11 +3,12 @@ Test Taxi Class
 """
 from prac_09.taxi import Taxi
 
-my_taxi = Taxi("Prius 1", 100, 1.23)
+my_taxi = Taxi(name="Prius 1", fuel=100)
 my_taxi.drive(40)
-print(Taxi)
+print(my_taxi)
 my_taxi.start_fare()
 my_taxi.drive(100)
+print(my_taxi)
 
 
 
