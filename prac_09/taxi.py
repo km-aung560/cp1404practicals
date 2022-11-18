aaed51f8d7247e58381761bdc3579812dd38ff95
km-aung560 <kaungmin.aung@my.jcu.edu.au>
2022-11-18 11:05:18 +1000

    Improve init function in taxi class

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -9,9 +9,9 @@ class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
     price_per_km = 1.23
 
-    def __init__(self, **kwargs):
+    def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
-        super().__init__(**kwargs)
+        super().__init__(name, fuel)
         self.current_fare_distance = 0
 
     def __str__(self):
